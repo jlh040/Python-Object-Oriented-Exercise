1eaa172530e7640233d95f6e72d29087a9f0395d
Jeff Hall <jeff.leon.hall@gmail.com>
2021-01-25 23:17:14 -0700

    defined the print_num_of_words method

--- wordfinder.py
+++ wordfinder.py
@@ -2,4 +2,15 @@
 
 
 class WordFinder:
-    ...
+    
+    def __init__(self, path):
+        self.path = path
+        self.list_of_words = []
+
+    def get_words(self):
+        words = open(self.path)
+        for word in words:
+            self.list_of_words.append(word)
+        
+    def print_num_of_words(self):
+        print(f'{len(self.list_of_words)} words read')
