7b55f965c87dba170beea5a07a06deaaf320a445
Jeff Hall <jeff.leon.hall@gmail.com>
2021-01-25 23:26:33 -0700

    defined the get_words method

--- wordfinder.py
+++ wordfinder.py
@@ -6,11 +6,13 @@ class WordFinder:
     def __init__(self, path):
         self.path = path
         self.list_of_words = []
+        self.get_words()
 
     def get_words(self):
-        words = open(self.path)
+        words = open(self.path, 'r')
         for word in words:
-            self.list_of_words.append(word)
+            self.list_of_words.append(word[:-1])
+        words.close()
         
     def print_num_of_words(self):
         print(f'{len(self.list_of_words)} words read')
