e061f023c6482c612e8e49924fe452ee2d0afbb8
Jeff Hall <jeff.leon.hall@gmail.com>
2021-01-26 00:42:11 -0700

    defined get_words and __init__

--- specialwordfinder.py
+++ specialwordfinder.py
@@ -1,2 +1,20 @@
 from wordfinder import WordFinder
 
+class SpecialWordFinder(WordFinder):
+    """Make a subclass of WordFinder."""
+
+    def __init__(self, path):
+        """Get all of the attributes from WordFinder and put it on the instance."""
+
+        super().__init__(path)
+    
+    def get_words(self):
+        """Get the words from the file and check for empty lines and octothorp
+        symbols.
+        """
+
+        words = open(self.path, 'r')
+        for word in words:
+            if word != '\n' and word[0] != '#':
+                self.list_of_words.append(word[:-1])
+        words.close()
